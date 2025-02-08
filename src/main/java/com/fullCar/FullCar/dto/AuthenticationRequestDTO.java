0dba53dd4b9e78dfff7ecb90262aed10bbfe47e3
hiagoCf19 <hiagoferreira@id.uff.br>
2025-02-08 19:21:32 -0300

    feat: configure swagger for auth and ad routes

--- src/main/java/com/fullCar/FullCar/dto/AuthenticationRequestDTO.java
+++ src/main/java/com/fullCar/FullCar/dto/AuthenticationRequestDTO.java
@@ -1,11 +1,14 @@
 package com.fullCar.FullCar.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
 
 public record AuthenticationRequestDTO(
+        @Schema(example ="teste@gmail.com")
         @Email
         String email,
+        @Schema(example = "123456")
         @NotNull
         String password) {
 }
